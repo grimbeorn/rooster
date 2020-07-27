1a16ac32c6242fb9f8f613b30d6c1fabf46fc117
grimbeorn <hernandomtz@gmail.com>
2020-07-26 23:35:48 -0500

    products 05

--- resources/views/faqs.blade.php
+++ resources/views/faqs.blade.php
@@ -18,18 +18,17 @@
     <br><br>
     <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>How </b></span> many tons can you supply?</h2>
     <p class="text-size-20 text-s-size-16 text-thin">
-      We can supply in various ways, please check this link
+      We can supply in various ways, please check this<a class="text-more-info text-green-hover" href="/products">link</a>.
     </p>
     <br><br>
     <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>When </b></span> is avocado season?</h2>
     <p class="text-size-20 text-s-size-16 text-thin">
-      At <b>Rooster</b> believe in the commitment of <b>daily work</b>, in long-term alliances and in the <b>well-being</b> of our business partners and collaborators.<br>
+    There are seasons in avocado tree bloom (Normal, Loca and aventajada) these give different qualities to the fruit through the seasons. The months with the lowest production rates are: March and April.<br>
     </p>
     <br><br>
     <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>Where</b></span> is it harvested?</h2>
     <p class="text-size-20 text-s-size-16 text-thin">
-      We know how important it is to receive <b>high quality products on time</b> and how it was agreed.
-      Our commitment is to offer you a shopping experience in which you only have to worry about selecting your products. The rest, <b>leave it to us</b>.
+      It's harvested in Michoacán, the avocado land.
     </p>
   </div>  
 </div>  
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -18,19 +18,35 @@
     <br><br>
     <h2 class="headline text-thin text-s-size-30"> Avocado Sizing</h2>
     <p class="text-size-20 text-s-size-16 text-thin">
-    By Air<br>
-    Carton boxes: 56 boxes of 11.30 kg<br>
-    Carton boxes: 104 boxes of 06.00 kg<br>
-    Carton boxes: 154 boxes of 04.00 kg<br>
-    Plastic boxes: 72 boxes of 10.00 kg<br>
-    By Sea<br>
-    Plastic boxes: 104 boxes of 10.00 kg<br>
-    Carton boxes: 88 boxes of 11.30 kg<br>
-    Carton boxes: 160 boxes of 06.00 kg<br>
-    Carton boxes: 264 boxes of 04.00 kg<br>
-    By Land<br>
-    Carton boxes: 80 boxes of 11.30 kg<br>
-    Carton boxes: 216 boxes of 0.00 kg<br>
+    + 330 gr<br>
+    European Caliber 12<br>
+    USA: 32<br>
+
+    300 - 330 gr<br>
+    European Caliber 14<br>
+    USA: 36<br>
+
+    265 - 300 gr<br>
+    European Caliber 16<br>
+    USA: 40<br>
+
+    205 - 265 gr<br>
+    European Caliber 18<br>
+    USA: 48<br>
+
+    170 - 205 gr<br>
+    European Caliber 22<br>
+    USA: 60<br>
+
+    150 - 170 gr<br>
+    European Caliber 24<br>
+    USA: 70<br>
+
+    120 - 150 gr<br>
+    European Caliber 26<br>
+    USA: 84<br>
+>
+   
     </p>
     <br><br>
     <h2 class="headline text-thin text-s-size-30"> Ripening</h2>
