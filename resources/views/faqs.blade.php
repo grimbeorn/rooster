d70f52f78cd4a6f0ee748678c2690a85f230ca65
grimbeorn <hernandomtz@gmail.com>
2020-07-26 22:53:05 -0500

    products 02

--- resources/views/faqs.blade.php
+++ resources/views/faqs.blade.php
@@ -11,24 +11,22 @@
 <div class="full-width text-left">
  <!--  <img class="center margin-bottom-30" style="margin-top: -210px; position:relative;" src="img/bsp/pngBsp23343391_005.png" alt=""> -->
   <div class="line">
-    <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>Our</b></span> Company</h2>
+    <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>What</b></span> certificates does mexican avocados has?</h2>
     <p class="text-size-20 text-s-size-16 text-thin">
-      <b>Rooster</b> is a <b>Mexican</b> company dedicated to the commercialization of Mexican hass avocado.<br>
-      Conscious of the commitment involved in delivering Mexican <b>products</b> abroad, we are concerned with bringing the highest standards of <b>quality</b>, cost and time to our clients.<br>
-      <i>* We only deliver full pallets of product.</i><br>
+      We count with the Global G.A.P., Primus GFS, USDA Organic, SMETA.
     </p>
     <br><br>
-    <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>Our</b></span> Product</h2>
+    <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>How </b></span> many tons can you supply?</h2>
     <p class="text-size-20 text-s-size-16 text-thin">
       Beyond the intrinsic quality of Mexican avocado, at <b>Rooster</b> we are concerned with making a selection of the <b>best products</b>, either for shipping avocado in bulk or for our processed products.<br>
     </p>
     <br><br>
-    <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>Our</b></span> Philosophy</h2>
+    <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>When </b></span> is avocado season?</h2>
     <p class="text-size-20 text-s-size-16 text-thin">
       At <b>Rooster</b> believe in the commitment of <b>daily work</b>, in long-term alliances and in the <b>well-being</b> of our business partners and collaborators.<br>
     </p>
     <br><br>
-    <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>Our</b></span> Commitment</h2>
+    <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>Where</b></span> is it harvested?</h2>
     <p class="text-size-20 text-s-size-16 text-thin">
       We know how important it is to receive <b>high quality products on time</b> and how it was agreed.
       Our commitment is to offer you a shopping experience in which you only have to worry about selecting your products. The rest, <b>leave it to us</b>.
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -7,10 +7,10 @@
 </header>
 
 <!-- Section 2 -->
-<section class="section background-white text-center"> 
+<section class="section background-white text-left"> 
   <div class="line">
     <div class="margin">
-      <div class="s-12 m-12 l-12 margin-m-bottom text-left">
+      <div class="s-12 m-12 l-12 margin-m-bottom">
         <h2 class="headline text-thin text-s-size-25"><span class="text-pink"></span> Avocado</h2>
         <p class="text-s-size-20">It's a mexican fruit, similar to a pear, rough green bark, smooth buttery pulp and large seed in the center..</p> 
         <!-- <a class="text-more-info text-green-hover" href="/">Read more</a>                 -->
@@ -43,31 +43,42 @@
   </div>
 </section>
 
-<!-- Section 2 -->
-<!-- <section class="section background-white text-center"> 
+<!-- Section 3 -->
+<section class="section background-white">
+<div class="full-width text-left">
+ <!--  <img class="center margin-bottom-30" style="margin-top: -210px; position:relative;" src="img/bsp/pngBsp23343391_005.png" alt=""> -->
   <div class="line">
-    <div class="margin">
-      <div class="s-12 m-12 l-4 margin-m-bottom">
-        <img class="margin-bottom" src="img/bsp/bsp192667750.jpg" alt="">
-        <h2 class="text-thin">Pulpa de Aguacate</h2>
-        <p>Pulpa de aguacate congelada, lista para utilizarse en cualquier receta.</p> 
-        <a class="text-more-info text-green-hover" href="/">Read more</a>                
-      </div>
-      <div class="s-12 m-12 l-4 margin-m-bottom">
-        <img class="margin-bottom" src="img/bsp/bsp326964397.jpg" alt="">
-        <h2 class="text-thin">Guacamole en Bolsa</h2>
-        <p>Receta clásica de guacamole (cebolla, tomate, cilantro, limón, un ligerísimo toque de picante y sal), congelado y empacado en bolsas de distintas presentaciones.</p> 
-        <a class="text-more-info text-green-hover" href="/">Read more</a>                
-      </div>
-      <div class="s-12 m-12 l-4 margin-m-bottom">
-        <img class="margin-bottom" src="img/bsp/bsp336594856.jpg" alt="">
-        <h2 class="text-thin">Guacamole en envase</h2>
-        <p>Receta clásica, congelado y empaquetado para consumo individual.</p> 
-        <a class="text-more-info text-green-hover" href="/">Read more</a>                
-      </div>
-    </div>
-  </div>
-</section> -->
+    <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>Rooster</b></span> Company</h2>
+    <p class="text-size-20 text-s-size-16 text-thin">
+      <b>Rooster</b> is a <b>Mexican</b> company dedicated to the commercialization of Mexican hass avocado.<br>
+      Conscious of the commitment involved in delivering Mexican <b>products</b> abroad, we are concerned with bringing the highest standards of <b>quality</b>, cost and time to our clients.<br>
+      <i>* We only deliver full pallets of product.</i><br>
+    </p>
+    <br><br>
+    <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>Rooster</b></span> Products</h2>
+    <p class="text-size-20 text-s-size-16 text-thin">
+      Beyond the intrinsic quality of Mexican avocado, at <b>Rooster</b> we are concerned with making a selection of the <b>best products</b>, either for shipping avocado in bulk or for our processed products.<br>
+    </p>
+    <br><br>
+    <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>Rooster</b></span> Philosophy</h2>
+    <p class="text-size-20 text-s-size-16 text-thin">
+      At <b>Rooster</b> believe in the commitment of <b>daily work</b>, in long-term alliances and in the <b>well-being</b> of our business partners and collaborators.<br>
+    </p>
+    <br><br>
+    <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>Rooster</b></span> Commitment</h2>
+    <p class="text-size-20 text-s-size-16 text-thin">
+      We know how important it is to receive <b>high quality products on time</b> and how it was agreed.
+      Our commitment is to offer you a shopping experience in which you only have to worry about selecting your products. The rest, <b>leave it to us</b>.
+    </p>
+   <br><br>
+   <h2 class="headline text-thin text-s-size-30"><span class="text-pink"><b>Why </b></span> Rooster?</h2>
+    <p class="text-size-20 text-s-size-16 text-thin">
+    It's easy: because the commitment we have with our clients and collaborators! <br>
+    </p>
+    <br><br> 
+  </div>  
+</div>  
+</section>
 
 
 @endsection
